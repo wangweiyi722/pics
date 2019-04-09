2e49aadffa7bc07350df79591a0995037e3b1ba7
Weiyi Wang <weiyiwang@google.com>
2019-04-09 09:29:54 -0500

    added css to imageList

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,10 @@
+import './ImageList.css'
 import React from 'react';
 
 const ImageList = (props) => {
-  console.log(props.images);
-  return <div>ImageList</div>
+  const images = props.images.map(({description, id, urls}) => {
+    return <img alt={description} key={id} src={urls.regular}/>
+  });
+  return <div className="image-list">{images}</div>
 }
 export default ImageList;
